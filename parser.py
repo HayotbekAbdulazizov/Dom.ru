02d56cac1639b42a706bc936518fcd270dc260e1
Hayotbek Abdulazizov <hayotbekabdulazizov2005@gmail.com.com>
2022-12-14 12:45:12 +0500

    parser is in progress offersection

--- parser.py
+++ parser.py
@@ -6,8 +6,8 @@ import time
 
 
 
-r = requests.get("https://dom.693006.ru/list?object=flat&deal=sell&page=1")
-globalSoup = BeautifulSoup(r.text, 'html.parser')
+globalSoupStatus = requests.get("https://dom.693006.ru/list?object=flat&deal=sell&page=1")
+globalSoup = BeautifulSoup(globalSoupStatus.text, 'html.parser')
 
 
 offerList = globalSoup.findAll("article", {"class": "list-card-desktop"})
@@ -17,9 +17,42 @@ offerList = globalSoup.findAll("article", {"class": "list-card-desktop"})
 
 
 
-for i in offerList:
-    i.find("a")
-    print(i.find("div", {"class": "list-card-address text-darkest-grey flex y-center"}).text)
+for i in offerList[0:2]:
+    title = None
+
+    offerPrice = None
+    offerPriceCurrency = None
+    offerPricePerMeter = None
+    offerAddress = None
+
+
+
+
+
+
+    
+    
+    mainOfferSectionRight = None
+    
+    
+    ahrefs = i.find("a")
+    detailSoupStatus = requests.get(f"https://dom.693006.ru{ahrefs['href']}")
+    detailSoup = BeautifulSoup(detailSoupStatus.text, "html.parser")
+    title = detailSoup.find("h1", {"class":"offer-announce offer-announce"}).text
+    
+    mainOfferSectionRight = detailSoup.find("article", {"class" : "offer-main"})
+    title = mainOfferSectionRight.find("h1", {"class":"offer-announce"}).text
+    offerAddress = mainOfferSectionRight.find("div", {"class":"offer-address block offer-address"}).text
+    offerStatus = mainOfferSectionRight.find("div", {"class":"offer-stats"})
+    print(offerAddress)
+    print(offerStatus)
+    
+    
+    
+    
+    print()
+    print()
+    # print(i.find("div", {"class": "list-card-address text-darkest-grey flex y-center"}))
 
 
 
@@ -27,6 +60,20 @@ for i in offerList:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# offer-main
 # list-card-desktop
 # content-padding
 # list-card-layout
