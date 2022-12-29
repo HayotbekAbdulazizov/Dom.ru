d07074e1df975d6be3758eca7f64c0ccc5d1a631
Hayotbek Abdulazizov <hayotbekabdulazizov2005@gmail.com.com>
2022-12-29 06:46:00 +0500

    celery is done

--- src/main/tasks.py
+++ src/main/tasks.py
@@ -6,6 +6,6 @@ from datetime import datetime
 
 @app.task #регистриуем таску
 def repeat_order_make():
-    post = Post.objects.create(title=f"Post - {datetime.now()}")
+    post = Post.objects.create(title=f"Post - {datetime.now()}", slug=f"slug-{datetime.now()}")
     print(f"{post.title} -- Post created")
     return "необязательная заглушка"
